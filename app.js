97b725f4384461228fad543e0d3436ed7c273f82
ipetty14 <ipetty@icloud.com>
2018-03-24 22:18:22 -0600

    Added lodash example

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('Starting app.js...');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 // var user = os.userInfo();
@@ -9,3 +10,7 @@ const notes = require('./notes.js');
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
 
 console.log('Result: ', notes.add(3, 5));
+
+console.log(_.isString('Ian'));
+
+console.log(_.uniq([1, 2, 3, 1, 2, 3, 4, 5, 6, 4, 5, 6]));
