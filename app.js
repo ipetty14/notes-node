898eab24f4961949e8787c6ed1a188f9168960d5
ipetty14 <ipetty@icloud.com>
2018-03-25 16:23:11 -0600

    Cleaned up examples to get started on notes app.

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-console.log('Starting app.js...');
+/*
+Project Title: Notes
+Author: Ian Petty
+*/
 
+// Built-in Packages
 const fs = require('fs');
-const os = require('os');
+
+// 3rd Party Packages
 const _ = require('lodash');
+
+// File Dependencies
 const notes = require('./notes.js');
-
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
-
-console.log('Result: ', notes.add(3, 5));
-
-console.log(_.isString('Ian'));
-
-console.log(_.uniq([1, 2, 3, 1, 2, 3, 4, 5, 6, 4, 5, 6]));
